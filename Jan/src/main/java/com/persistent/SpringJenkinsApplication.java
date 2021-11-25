23e4f12bf364975cde5e92fa6e3c1c14aec6015b
Ameer Mohammad <ameer_mohammad@persistent.com>
2021-11-25 16:47:38 +0530

    maven project

---
+++ Jan/src/main/java/com/persistent/SpringJenkinsApplication.java
@@ -0,0 +1,28 @@
+package com.persistent;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplication.class);
+
+	@PostConstruct
+	public void init() {
+		logger.info("Welcome to Jenkins Demo Application Started");
+
+	}
+
+	public static void main(String[] args) {
+		logger.info("application Executed sucessfully");
+		
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+		System.out.println("----Welcome----");
+	}
+
+}
